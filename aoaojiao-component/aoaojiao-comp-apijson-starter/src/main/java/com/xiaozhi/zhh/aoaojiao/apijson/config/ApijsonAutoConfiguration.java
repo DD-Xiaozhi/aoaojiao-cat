ad7b985a693e35eb911f7d26abdee5e1e31d18a8
DD <ddxu@chatlabs.cn>
2025-03-23 13:04:57 +0800

    feat(component): 添加 APIJSON 组件
    - 新增 aoaojiao-comp-apijson-starter 模块
    - 添加 APIJSON 相关依赖
    - 创建 ApijsonAutoConfiguration 配置类
    - 更新父 POM 文件，添加新模块引用
    - 在根 POM 文件中添加 APIJSON 版本号和 JitPack 仓库
    - 新增 spring.factories 文件，注册自动配置类

---
+++ aoaojiao-component/aoaojiao-comp-apijson-starter/src/main/java/com/xiaozhi/zhh/aoaojiao/apijson/config/ApijsonAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.xiaozhi.zhh.aoaojiao.apijson.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author DD
+ * @date 2025-03-23
+ */
+@Configuration
+public class ApijsonAutoConfiguration {
+
+
+}
